a75b6b39b90009a1ec98462e6a342ab1495cf7ae
中度 <1422315913@qq.com>
2022-08-05 01:16:25 +0800

    首页样式编写

---
+++ App.vue
@@ -0,0 +1,25 @@
+<script>
+	export default {
+		onLaunch: function() {
+			console.log('App Launch')
+		},
+		onShow: function() {
+			console.log('App Show')
+		},
+		onHide: function() {
+			console.log('App Hide')
+		}
+	}
+</script>
+
+<style lang="scss">
+	/* UI框架 */
+	@import "static/styles/uni.css";
+	@import "colorui/main.css";
+	@import "colorui/icon.css";
+	@import "uview-ui/index.scss";
+	
+	/* 全局样式 */
+	@import "static/styles/global.css";
+	/*每个页面公共css */
+</style>
---
+++ pages/index/index.vue
@@ -0,0 +1,50 @@
+<template>
+	<view class="indexContent">
+		<div class="searchAndMessage">
+			<view class="cu-bar search">
+				<view class="search-form radius">
+					<text class="cuIcon-search"></text>
+					<input disabled type="text" placeholder="点击搜索" @click="goSearch"></input>
+				</view>
+				<view class="action">
+					<u-badge bgColor="#B22222" count="999" :offset="[13,25]"></u-badge>
+					<text class="cuIcon-message message" @click="goMessage"></text>
+				</view>
+			</view>
+		</div>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+			goSearch() {
+				uni.navigateTo({
+					url: '/pages/search/search'
+				})
+			},
+			goMessage() {
+				uni.navigateTo({
+					url: '/pages/message/message'
+				})
+			}
+		}
+	}
+</script>
+
+<style lang="scss" scoped>
+	.search {
+		background-color: $bc-color;
+	}
+	.message::before {
+		font-size: 26px;
+	}
+</style>
